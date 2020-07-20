0489d0d73a38053925c91c528f2cda203e6455e5
JudieDai <870869788@qq.com>
2020-07-20 18:55:37 +0800

    data preprocess copy from LGSR

--- cmain.py
+++ cmain.py
@@ -1,2 +1,14 @@
+import pickle
+
+def _testyield():
+    for i in range(5):
+        yield i
+
+def main():
+    generator = _testyield()
+    for i in generator:
+        print(i)
+
+
 if __name__ == "__main__":
-    print("hello world")
+    main()
